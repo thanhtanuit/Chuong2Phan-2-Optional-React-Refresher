f218df5b7a665b553eedb729c6ea5617088cae21
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 12:38:45 +0700

    11-conditional-content

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -4,9 +4,13 @@ import classes from "./PostsList.module.css";
 import { useState } from "react";
 import Modal from "./Modal";
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
@@ -16,12 +20,14 @@ function PostsList() {
   }
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
 
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
