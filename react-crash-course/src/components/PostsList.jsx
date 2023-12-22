d7717ee6c18cf62476a493aa8f02cb1d8cce6827
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 11:23:32 +0700

    07-event-listener

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,8 +1,10 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 function PostsList() {
   return (
     <>
+      <NewPost />
       <ul className={classes.posts}>
         <Post author="Maximilian" body="React.js is awesome!" />
         <Post author="Manuel" body="Check out the full course!" />
