8a606e3cc83f34721b890ad9364183c59a0e0f6d
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 10:41:16 +0700

    04-props

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import Post from "./components/Post";
 
 export default function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Maximilian" body="React.js is awesome!" />
+      <Post author="Manuel" body="Check out the full course!" />
+    </main>
+  );
 }
