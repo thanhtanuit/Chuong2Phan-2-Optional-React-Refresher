1d80ac4d0e90aa3c0bda97582efd58bd87688582
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 13:31:54 +0700

    14-data-fetching-useeffect

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,11 +1,26 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./Modal";
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:5000/vite/GetVites");
+      const resData = await response.json();
+      setPosts(resData.data);
+    }
+    fetchPosts();
+  }, []);
   function addPostHandler(postData) {
+    fetch("http://localhost:5000/vite/PostVite", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
