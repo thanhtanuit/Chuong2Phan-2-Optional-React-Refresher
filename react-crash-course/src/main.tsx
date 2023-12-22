6384476d07a86d512fa06f2d140fb1ff489aedbd
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 13:43:52 +0700

    16-added-routing

--- react-crash-course/src/main.tsx
+++ react-crash-course/src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import NewPost from "./components/NewPost.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/create-post", element: <NewPost /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
