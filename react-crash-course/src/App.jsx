1bf7d1be4d84e0323e2b15f4d25aa6f6abd0a204
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 11:19:07 +0700

    06-exercise-solution-postlist-cmp

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Post from "./components/Post";
+import PostsList from "./components/PostsList";
 
 export default function App() {
   return (
     <main>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check out the full course!" />
+      <PostsList />
     </main>
   );
 }
