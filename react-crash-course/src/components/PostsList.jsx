77490f071bf9684f08b058d1aea8d5241a721041
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 13:22:44 +0700

    13-outputting-a-list-of-posts

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -4,16 +4,22 @@ import classes from "./PostsList.module.css";
 import { useState } from "react";
 import Modal from "./Modal";
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author="author" body="body" />
+        {posts.map((e) => {
+          return <Post author={e.author} body={e.body} />;
+        })}
       </ul>
     </>
   );
