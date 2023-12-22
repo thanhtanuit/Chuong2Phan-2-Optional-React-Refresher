bd51ee852cb71859a9bb0268c3fcd6460a677db6
Pham Thanh Tan <tanpt@bidc.vn>
2023-12-22 13:17:17 +0700

    12-form-submision

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
 import Post from "./components/Post";
 import PostsList from "./components/PostsList";
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState();
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   return (
     <main>
-      <PostsList />
+      <MainHeader onCreatePost={showModalHandler} />
+      <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
     </main>
   );
 }
--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -3,34 +3,17 @@ import Post from "./Post";
 import classes from "./PostsList.module.css";
 import { useState } from "react";
 import Modal from "./Modal";
-function PostsList() {
-  const [modalIsVisible, setModalIsVisible] = useState(true);
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
+function PostsList({ isPosting, onStopPosting }) {
   return (
     <>
-      {modalIsVisible && (
-        <Modal onClose={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="author" body="body" />
       </ul>
     </>
   );
